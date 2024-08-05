c541c49af1064dd4358e57e5267f46f9918cf958
Matheus Henrique Dias do Nascimento <matheus.mhddn@gmail.com>
2024-08-05 10:27:52 -0300

    feat: create users table migration file

--- src/database/knex/migrations/20240802203601_migrations_test.js
+++
@@ -1,7 +0,0 @@
-exports.up = (knex) =>
-  knex.schema.createTable("sampleTable", (table) => {
-    table.increments("id");
-    table.string("title");
-  });
-
-exports.down = (knex) => knex.schema.createTable("sampleTable", {});

c1255ca6c44cda82df21766f08d00a1f5b6306a6
Matheus Henrique Dias do Nascimento <matheus.mhddn@gmail.com>
2024-08-04 21:46:54 -0300

    feat: create `AppError` handle at `server.js`

--- src/server.js
+++ src/server.js
@@ -1,11 +1,29 @@
+require("express-async-errors");
 const express = require("express");
 const routes = require("./routes");
+const AppError = require("./utils/AppError");
 
 const app = express();
 
 app.use(routes);
 
 const port = 3333;
+
+app.use((req, res, next, error) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+
+  console.error(error);
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

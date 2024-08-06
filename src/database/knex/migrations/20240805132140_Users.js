add12085b693d6b520f3247968353d0980a4e0de
Matheus Henrique Dias do Nascimento <matheus.mhddn@gmail.com>
2024-08-06 11:22:06 -0300

    fix: fix project code format

--- src/database/knex/migrations/20240805132140_Users.js
+++ src/database/knex/migrations/20240805132140_Users.js
@@ -1,5 +1,5 @@
-exports.up = knex =>
-  knex.schema.createTable("users", table => {
+exports.up = (knex) =>
+  knex.schema.createTable("users", (table) => {
     table.increments("id");
     table.text("name").notNullable();
     table.text("email").notNullable();
@@ -9,4 +9,4 @@ exports.up = knex =>
   });
 
 // eslint-disable-next-line no-unused-vars
-exports.down = knex => knex.schema.dropTable("Users", table => {});
+exports.down = (knex) => knex.schema.dropTable("Users", (table) => {});
